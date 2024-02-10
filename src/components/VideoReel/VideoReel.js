1f50f06abf72844176106bef22c5b523d4c0fa42
adrian <adrian>
2024-02-10 04:17:50 -0600

    Creating the VideoReel component. By using virtualization with the React Window library, we created an infinite scroll of video card elements that later will be used to display short videos.

---
+++ src/components/VideoReel/VideoReel.js
@@ -0,0 +1,31 @@
+import { h, render } from "preact"
+import { FixedSizeList as List } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
+import "./VideoReel.css"
+
+const Row = ({ index, style }) => (
+  <div className="videoCard" style={style} > {index}</div>
+)
+
+const Demo = () => {
+  return(
+    <div className = "videoReelContainer">
+      <AutoSizer>
+        {({ height, width }) => (
+          <List
+          className = "shortVideoReel"
+          height = { height }
+          itemCount = { 1000 }
+          itemSize = { height }
+          width = { width }
+          >
+            { Row }
+          </List>
+        )}
+      </AutoSizer>
+    </div>
+  )
+}
+
+  
+render(<Demo />, document.getElementById("infiniteScrollOfShortVideos"));
