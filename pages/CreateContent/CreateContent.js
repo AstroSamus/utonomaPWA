4a4f3da6ba0f4cf0a939c023d534a9b05fbe9149
adrian <adrian>
2024-02-20 22:43:03 -0600

    Created step two from the Create Content stepper menu

--- pages/CreateContent/CreateContent.js
+++ pages/CreateContent/CreateContent.js
@@ -1,1 +1,29 @@
-console.log("holaaafdfafsfefasf from webpack")
+document.querySelector("#button-short-video-input").addEventListener("click", () => {
+  $shortVideoInput.click()
+})
+
+const $shortVideoInput = document.querySelector("#short-video-input")
+const $stepOne = document.querySelector("#step-1")
+const $stepTwo = document.querySelector("#step-2")
+const $stepTwoVideo = document.querySelector("#step-2-video")
+const $stepThree = document.querySelector("#step-3")
+
+
+$shortVideoInput.addEventListener("change", () => {
+  if ($shortVideoInput.files.length == 1) {
+    $stepOne.style.display = "none"
+    $stepTwo.style.display =  "flex"
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $stepTwoVideo.src = e.target.result
+      $stepTwoVideo.play()
+    }.bind(this)
+    reader.readAsDataURL($shortVideoInput.files[0]);
+  }
+})
+
+document.querySelector("#button-continue-step-2").addEventListener("click", () => {
+  $stepTwo.style.display = "none"
+  $stepThree.style.display = "flex"
+  $stepTwoVideo.pause()
+})
