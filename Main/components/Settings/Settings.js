9552abe4e942eb1c9766adbeba61582e1e6a7eb6
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2024-12-03 13:18:58 -0600

    Adding the token to the wallet pressing a button. In the Configure your Wallet button, we inclued another button that adds the token to the modal, for now, only works in brave or metamask, not by wallet connect.

--- Main/components/Settings/Settings.js
+++ Main/components/Settings/Settings.js
@@ -36,6 +36,38 @@ $buttonSendTokens.addEventListener('click', () => {
   $dialogSendOrReceiveTokens.showModal()
 })
 
+$buttonDialogAddTokenAutomatically.addEventListener('click' , async() => {
+  const { 
+    utonomaSepoliaAddress,
+    sepoliaTokenSymbol,
+    tokenDecimals
+  } = await import('../../../utonomaSmartContract.js')
+  try {
+    // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+    const wasAdded = await ethereum.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20', // Initially only supports ERC20, but eventually more!
+        options: {
+          address: utonomaSepoliaAddress, // The address that the token is at.
+          symbol: sepoliaTokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+          decimals: tokenDecimals, // The number of decimals in the token
+          //image: , // A string url of the token logo
+        },
+      },
+    })
+  
+    if (wasAdded) {
+      console.log('Thanks for your interest!');
+    } else {
+      console.log('Your loss!');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+})
+
 $buttonDialogCloseSendTokens.addEventListener('click', () => {
   $dialogSendOrReceiveTokens.close()
 })
