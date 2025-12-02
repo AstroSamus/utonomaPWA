e770bed82c574584ee2e08ab0fd47e86669d5cf7
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2025-12-02 14:30:57 -0600

    Created a new userManager object. It centralizes the logic for persistently tracking whether the user is currently connected or has ever been connected. Previously, we struggled to determine connection status because wallets can disconnect at any time. In this new implementation, we assume the user is logged in if they have connected their account at any point—even if the wallet subsequently closes the connection.

--- services/userManager/userManager.js
+++ services/userManager/userManager.js
@@ -4,6 +4,37 @@ const addressKey = 'userAddress'
 let isLoggedIn
 let address
 
+export const USER_ADDRESS_KEY = 'userAddress'
+
+export const userManager = {
+  _isLoggedIn: null,
+  _lastKnownUserAddress: localStorage[USER_ADDRESS_KEY] ?? null,
+
+  get lastKnownUserAddress() {
+    return this._lastKnownUserAddress
+  },
+  
+  get isLoggedIn() { 
+    return !!this._lastKnownUserAddress;
+  },
+
+  set lastKnownUserAddress(address) {
+    this._lastKnownUserAddress = address
+    localStorage[USER_ADDRESS_KEY] = address
+    window.dispatchEvent(
+      new StorageEvent('storage', {
+        key: USER_ADDRESS_KEY,
+        value: address,
+      })
+    )
+  },
+
+  /*
+  logout() {
+    this.lastKnownUserAddress = null
+  },*/
+}
+
 export function getIsLoggedIn() {
   if(!isLoggedIn) {
     const isLoggedInFromLocalStorage = localStorage[isLoggedInKey]
@@ -43,7 +74,5 @@ export function setAddress(newValue) {
   if(typeof newValue != 'string') throw 'Invalid type for the address value'
   if(address !== newValue) {
     address = newValue
-    localStorage[addressKey] = newValue
-    window.dispatchEvent(new StorageEvent('storage', { key: addressKey }))
   }
 }
