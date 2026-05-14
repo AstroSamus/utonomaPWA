fff7f9067af7f9590a95c4335bc0052ab983c1e7
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2026-05-14 14:45:45 -0600

    Installing and configuring HtmlCriticalWebpackPlugin to inline critical css above the fold. We installed this plugin to improve performance only for production builds., Have a closer look into what it is doing as we are experimenting with this.

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -2,9 +2,28 @@ const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.cjs')
 const TerserPlugin = require('terser-webpack-plugin');
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, 'dist'),
+      src: 'index.html',
+      dest: 'index.html',
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 430, //iphone 14 pro max size
+      height: 935,
+      penthouse: {
+        blockJSRequests: false,
+      },
+      ignore: {
+        atrule: ['@font-face'],
+      },
+    })
+  ],
   resolve: {
     alias: {
       'config.env': path.resolve(__dirname, 'config.prod.js'),
