672750ed13d2db4e0c7b34413f4300fbbec50dd3
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2026-05-17 11:33:21 -0600

    Adding jsDocs documentation to the Dialog component.

--- components/Dialog/Dialog.js
+++ components/Dialog/Dialog.js
@@ -1,10 +1,28 @@
+/**
+ * @typedef {Object} DialogProps
+ * @property {string} title
+ * @property {string} text
+ * @property {string} cancelText
+ * @property {string} acceptText
+ */
+
+/**
+ * @typedef {Object} DialogVariant
+ * @property {'warning'|'success'|'normal'} severity
+ * @property {'decision'|'alert'|'toast'} intention
+ */
+
+/**
+ * @param {HTMLDialogElement} $dialog
+ * @param {DialogProps} props
+ * @param {DialogVariant} variant
+ */
 export const Dialog = ($dialog, props, variant) => {
-  
   const {
     title,
     text,
-    cancel,
-    accept
+    cancelText,
+    acceptText
   } = props
 
   const {
@@ -12,20 +30,23 @@ export const Dialog = ($dialog, props, variant) => {
     intention
   } = variant
 
+  const $buttonAccept = $dialog.querySelector('[data-id="button-accept"]')
+  const $buttonCancel = $dialog.querySelector('[data-id="button-cancel"]')
+
   $dialog.querySelector('div:first-child').innerText = title
   $dialog.querySelector('p').innerText = text
-  $dialog.querySelector('button:last-child').innerText = accept
-  $dialog.querySelector('button:first-child').innerText = cancel
+  $buttonAccept.innerText = acceptText
+  $buttonCancel.innerText = cancelText
 
   switch (severity) {
-    case 'WARNING':
+    case 'warning':
       $dialog.classList.add('dialog--warning', 'dialog--decision')
       break;
   }
   switch (intention) {
-    case 'DECISION' :
+    case 'decision' :
       break
-    case 'ALERT' :
+    case 'alert' :
       $buttonAccept.style.display = 'none'
       break
   }
@@ -35,8 +56,13 @@ export const Dialog = ($dialog, props, variant) => {
     alert: () => {
       $dialog.showModal()
     },
+    /**
+     *  Opens the dialog and waits for the user's decision.
+     *  @returns {Promise<boolean>} `true` if the user accepts the dialog, otherwise `false`
+     * 
+     */
     ask: () => {
-      if(variant?.intention !== 'DECISION') {
+      if(variant.intention !== 'decision') {
         throw new Error('Cannot use ask if dialog intention is not DECISION')
       }
 
@@ -52,6 +78,7 @@ export const Dialog = ($dialog, props, variant) => {
         }, { once: true })
       })
     },
+    /** @param {number} miliseconds The amount of miliseconds that the toast will show*/
     toast: (miliseconds) => {
       $dialog.showModal()
       setTimeout(() => {
