11a680cc78a90b7d8c5b2c8238a2d4f341f4e77e
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2026-06-04 23:29:23 -0600

    Created new HideableContainer utility. It allows to create a wrapper and toggle visibility between all the childs without having layout shifting

---
+++ components/utils/HideableContainer/HideableContainer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {HTMLElement} $container Any HTML tag that encloses the elements 
+ * whose visibility will be toggled.
+ */
+export const HideableContainer = ($container) => {
+  //all hidden when component starts
+  const $childs = [...$container.children]
+  $childs.forEach(el => el.style.visibility = 'hidden')
+
+  return {
+    /**
+     * @param {string} query A valid CSS selector that identifies the element 
+     * to be made visible.
+     */
+    show: (query) => {
+      $childs.forEach(el => {
+        if(el instanceof HTMLElement) {
+          el.style.visibility = 'hidden'
+        }
+      })
+      
+      const $target = $container.querySelector(query)
+      if($target instanceof HTMLElement) target.style.visibility = 'visible'
+    }
+  }
+
+}
