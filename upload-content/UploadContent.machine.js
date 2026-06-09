a303d2776b0ae121b1e720eb0c4a7ba73cb84ce3
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2026-06-09 12:12:37 -0600

    Created UploadContent state machine. Is an object with getters and setters that only allows the state to change to a new valid and known state.

---
+++ upload-content/UploadContent.machine.js
@@ -0,0 +1,43 @@
+/**
+ * @typedef {'start' | 'validatingWallet' | 'creatingUploadSession' | 'insuficientFunds' |  'walletError' | 'unexpectedError' } UploadContentMachineStates
+ */
+
+export const UploadContentMachine = {
+  /**@type UploadContentMachineStates */
+  _state: 'start',
+  _effects: {},
+
+  /**@param {UploadContentMachineStates} newState */
+  set state(newState) {
+    if(this._state === 'start' && newState !== 'validatingWallet' ) { 
+      throw new Error(`State ${this._state} cannot transition to ${newState}`)
+    }
+    if(this._state === 'validatingWallet' 
+      && (
+        newState !== 'insuficientFunds'
+        && newState !== 'walletError'
+        && newState !== 'creatingUploadSession'
+        && newState !== 'unexpectedError'
+      )
+    ) { throw new Error(`State ${this._state} cannot transition to ${newState}`)}
+    if(this._state === 'walletError' 
+      && (
+        newState !== 'validatingWallet'
+        //&& newState !== 'walletError'
+        //&& newState !== 'creatingUploadSession'
+      )
+    ) { throw new Error(`State ${this._state} cannot transition to ${newState}`)}
+    
+    this._state = newState
+    if(this._effects[this._state]) {
+      this._effects[this._state]()
+    }
+  },
+  get state() {
+    return this._state
+  },
+
+  set effects(effects) {
+    this._effects = effects
+  }
+}
