1a77cb338f85d42318c5a68ff63a2f48af3652d4
Adrian Armenta Sequeira <adrian.sequeira@utonoma.com>
2026-06-09 15:33:03 -0600

    Modifiying the apiUrl in the config constants to be only the base url

--- config.prod.js
+++ config.prod.js
@@ -2,7 +2,7 @@ export const isDevEnvironment = false
 
 export const rpcUrl = 'https://api.avax.network/ext/bc/C/rpc'
 
-export const apiUrl = 'http://localhost:3000/upload-content/create-upload-session'
+export const apiUrl = 'http://localhost:3000/'
 
 export const ipfsJsonUploaderUrl = 'https://mmcvhj7mz9.us-east-1.awsapprunner.com/pinJsonToIpfs'
 
